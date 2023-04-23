28d199d773859d2a8f9e7d260b97b896802b6f0b
Amal-Mousa <amalalsattari@gmail.com>
2023-04-23 15:34:31 +0300

    remove-comments

--- notes.js
+++ notes.js
@@ -151,3 +151,25 @@
 //?* return undefiend if we dont return post.rows[0] else return specific post details
 //     .then((data) => console.log(data))
 // };
+
+//! protected routes
+//* take token , verify it , next if there is no errors and next(err) if there is an error
+//* we call checkAuth middleware bcz it will come before routes.
+
+// const { verifyToken, CustomError } = require('../utils');
+
+// const checkAuth = (req, res, next) => {
+//   const { token } = req.cookies;
+//   //* ! not logged in
+//   if (!token) {
+//     throw new CustomError(401, 'Unauthorized');
+//   }
+//   //* ! to verify token if its valid or not(edit, expired)
+//   verifyToken(token)
+//     .then(decoded => {
+//       req.user = decoded;
+//       //* ! transfer to the next controller
+//       next();
+//     })
+//     .catch((err) => next(err));
+// };
